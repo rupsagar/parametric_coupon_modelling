ea71323a24c0b12bfff57c9782346981efcb3308
Rupsagar Chatterjee <22033397+rupsagar@users.noreply.github.com>
2023-02-08 10:14:19 +0530

    v1

---
+++ Scripts/src/util/util_call_abaqus.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import importlib
+import json
+import ast
+
+srcPath = sys.argv[-1]
+savePath = sys.argv[-2]
+template = sys.argv[-3]
+tempDataFileName = sys.argv[-4]
+statusFileName = sys.argv[-5]
+
+os.chdir(savePath)
+
+fileJson = open(savePath+'/'+tempDataFileName)
+couponDatabaseUnicode = json.load(fileJson)
+fileJson.close()
+couponDatabase = ast.literal_eval(json.dumps(couponDatabaseUnicode))
+
+if os.path.exists(savePath+'/'+statusFileName):
+    os.remove(savePath+'/'+statusFileName)
+statusFile = open(savePath+'/'+statusFileName, 'w')
+
+textSuccess = 'SUCCESS! Model created successfully.'
+textFail = 'FAILED! Model cannot be created.'
+
+sys.path.append(srcPath+'/class')
+couponDef = getattr(importlib.import_module('class_'+template.lower()), template.lower())
+try:
+    couponDef(couponDatabase)
+    statusFile.write(textSuccess)
+except Exception as e:
+    statusFile.write(textFail+'\nError Message: '+str(e))
+os.remove(srcPath+'/class/class_'+template.lower()+'.pyc')
+
+statusFile.close()
+sys.exit()
