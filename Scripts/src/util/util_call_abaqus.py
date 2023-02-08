a7dfc1d90211fc579545d7cc004519b89d34d255
Rupsagar Chatterjee <22033397+rupsagar@users.noreply.github.com>
2023-02-08 23:25:36 +0530

    v3

--- Scripts/src/util/util_call_abaqus.py
+++ Scripts/src/util/util_call_abaqus.py
@@ -10,29 +10,26 @@ template = sys.argv[-3]
 tempDataFileName = sys.argv[-4]
 statusFileName = sys.argv[-5]
 
-savePath = savePath.replace(' ', '\\ ')
-os.chdir(savePath)
-
-fileJson = open(savePath+'/'+tempDataFileName)
-couponDatabaseUnicode = json.load(fileJson)
-fileJson.close()
-couponDatabase = ast.literal_eval(json.dumps(couponDatabaseUnicode))
-
-if os.path.exists(savePath+'/'+statusFileName):
-    os.remove(savePath+'/'+statusFileName)
+textSuccess = r'SUCCESS! Model created successfully.'
+textFail = r'FAILED! Model cannot be created.'
 statusFile = open(savePath+'/'+statusFileName, 'w')
 
-textSuccess = 'SUCCESS! Model created successfully.'
-textFail = 'FAILED! Model cannot be created.'
-
-sys.path.append(srcPath+'/class')
-couponDef = getattr(importlib.import_module('class_'+template.lower()), template.lower())
 try:
-    couponDef(couponDatabase)
+    fileJson = open(savePath+'/'+tempDataFileName, 'r')
+    couponDataUnicode = json.load(fileJson)
+    fileJson.close()
+    couponData = ast.literal_eval(json.dumps(couponDataUnicode))
+    
+    sys.path.append(srcPath+'/class')
+    couponDef = getattr(importlib.import_module('class_'+template.lower()), template.lower())
+    os.chdir(savePath)
+    couponDef(couponData)
     statusFile.write(textSuccess)
-except Exception as e:
-    statusFile.write(textFail+'\nError Message: '+str(e))
-os.remove(srcPath+'/class/class_'+template.lower()+'.pyc')
+except Exception as err:
+    statusFile.write(textFail+'\nError Message: '+str(err))
+
+if os.path.exists(srcPath+'/class/class_'+template.lower()+'.pyc'):
+    os.remove(srcPath+'/class/class_'+template.lower()+'.pyc')
 
 statusFile.close()
 sys.exit()
