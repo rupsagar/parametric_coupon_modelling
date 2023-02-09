8be0593735c48a041540598036f86144a9fb5164
Rupsagar Chatterjee <22033397+rupsagar@users.noreply.github.com>
2023-02-10 02:05:03 +0530

    v4
    the problem with caching is removed; sys.path process of importing module is dropped and instead imp module is used.

--- Scripts/src/util/util_debug.py
+++ Scripts/src/util/util_debug.py
@@ -1,50 +1,30 @@
-srcPath = r'Z:\Rupsagar\04_Coupon_Parametric_Modelling\01_WIP\Scripts\src'
+## modify these 3 lines for debugging purpose and copy/paste (or run) in Abaqus CLI
+srcPath = r'D:\Academics\Programming\python\abaqus_scripts\Coupon_Parametric_Modelling\Scripts\src'
 template = 'Coupon_70_73_BE'
 coupon = 'Coupon_70C'
 
 import json
 import ast
-import sys
-import importlib
+import imp
 import os
 import shutil
 
-# try:
-databaseJson = open(srcPath+'/database/database_coupon.json', 'r')
-databaseUnicode = json.load(databaseJson)
-databaseJson.close()
-couponDatabase = ast.literal_eval(json.dumps(databaseUnicode))
-couponData = couponDatabase[template][coupon]
+try:
+    databaseJson = open(srcPath+'/database/database_coupon.json', 'r')
+    databaseUnicode = json.load(databaseJson)
+    databaseJson.close()
+    couponDatabase = ast.literal_eval(json.dumps(databaseUnicode))
+    couponData = couponDatabase[template][coupon]
 
-def convertJsonDataToStr(dictData):
-    for key, val in dictData.items():
-        if isinstance(val, dict):
-            convertJsonDataToStr(val)
-            continue
-        if not isinstance(val, str):
-            dictData[key] = str(val)
-    return dictData
+    couponModule = imp.load_source('class_'+template.lower(), srcPath+'/class/class_'+template.lower()+'.py')
+    couponClass = getattr(couponModule, template.lower())
+    self = couponClass(couponData)
 
-import imp
+    if os.path.exists(srcPath+'/class/class_'+template.lower()+'.pyc'):
+        os.remove(srcPath+'/class/class_'+template.lower()+'.pyc')
 
-def createCoupon():
-    couponDataStr = convertJsonDataToStr(couponData)
-    sys.path.append(srcPath+'/class')
-    foo = imp.load_source('class_'+template.lower()+'.py', srcPath+'/class/class_'+template.lower()+'.py')
-    couponDef = getattr(importlib.import_module('class_'+template.lower()), template.lower())
-    foo_class = getattr(foo, template.lower())
-    coupon = couponDef(couponDataStr)
-    foo_class(couponDataStr)
-    return coupon
-
-self = createCoupon()
-
-# except Exception as err:
-#     print(str(err))
-
-if os.path.exists(srcPath+'/class/class_'+template.lower()+'.pyc'):
-    os.remove(srcPath+'/class/class_'+template.lower()+'.pyc')
-
-if os.path.isdir(srcPath+'/class/__pycache__'):
-    shutil.rmtree(srcPath+'/class/__pycache__')
+    if os.path.isdir(srcPath+'/class/__pycache__'):
+        shutil.rmtree(srcPath+'/class/__pycache__')
+except Exception as err:
+    print(str(err))
 
