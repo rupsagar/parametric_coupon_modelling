476145f153f147fc8768a6265011b26ba9971eb7
Rupsagar Chatterjee <22033397+rupsagar@users.noreply.github.com>
2023-02-13 17:45:04 +0530

    v4

--- Scripts/src/main_file.py
+++ Scripts/src/main_file.py
@@ -1,20 +1,12 @@
-## may need to modify this path depending on local machine or OS
-abqPath = 'C:/SIMULIA/Commands'
+## user may need to modify this path depending on local machine or OS
+abqPath = [r'C:/SIMULIA/Commands']
 
 import os
 import imp
-import json
-import ast
 
 srcPath = os.getcwd()
-
-## read coupon json database
-couponJson = open(srcPath+'/database/database_coupon.json', 'r')
-couponDictUnicode = json.load(couponJson)
-couponJson.close()
-couponDict = ast.literal_eval(json.dumps(couponDictUnicode))
 
 ## create gui window
 guiModule = imp.load_source('class_coupon_gui', srcPath+'/class/class_coupon_gui.py')
 guiClass = getattr(guiModule, 'coupon_gui')
-newGUI = guiClass(abqPath, srcPath, couponDict)
+newGUI = guiClass(abqPath, srcPath)
