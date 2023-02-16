48623cad54a81e341b57209cd010bb04371df699
Rupsagar Chatterjee <22033397+rupsagar@users.noreply.github.com>
2023-02-16 22:04:09 +0530

    v2

--- Scripts/src/utils/util_debug.py
+++ Scripts/src/utils/util_debug.py
@@ -1,14 +1,33 @@
-## modify these 3 lines for debugging purpose and copy/paste (or run) in Abaqus CLI; the list contains possible paths for src folder
-template = 'Static_Coupon_1_1_7'
-coupon = 'Static_Coupon_1_1'
+## Modify the template and coupon lines for debugging purpose and copy+paste or run the script in Abaqus CLI
+## The srcPath list contains possible paths for src folder which may need to be updated based on where the script is located
 srcPath = [r'Z:\Rupsagar\04_Coupon_Parametric_Modelling\01_WIP\Scripts\src', 
-r'D:\Programming\python\abaqus_scripts\Coupon_Parametric_Modelling\Scripts\src']
+        r'D:\Programming\python\abaqus_scripts\Coupon_Parametric_Modelling\Scripts\src']
+
+# template = 'Fatigue_Coupon_66_69_A'
+# coupon = 'Fatigue_Coupon_67A'
+
+# template = 'Fatigue_Coupon_66_69_BJ'
+# coupon = 'Fatigue_Coupon_66C'
+
+# template = 'Fatigue_Coupon_70_73_A'
+# coupon = 'Fatigue_Coupon_70A'
+
+# template = 'Fatigue_Coupon_70_73_BE'
+# coupon = 'Fatigue_Coupon_70C'
+
+# template = 'Fatigue_Coupon_74_75'
+# coupon = 'Fatigue_Coupon_74'
+
+# template = 'Static_Coupon_1_1_7'
+# coupon = 'Static_Coupon_1_5'
+
+# template = 'Static_Coupon_2_1_6'
+# coupon = 'Static_Coupon_2_4'
 
 import json
 import ast
-import imp
 import os
-import shutil
+import sys
 
 try:
     for i in range(len(srcPath)):
@@ -22,16 +41,18 @@ try:
     couponDatabase = ast.literal_eval(json.dumps(databaseUnicode))
     couponData = couponDatabase[template][coupon]
     
-    ## load current coupon module
-    couponModule = imp.load_source('class_'+template.lower(), srcPath[srcPathID]+'/classes/class_'+template.lower()+'.py')
-    couponClass = getattr(couponModule, template.lower())
+    currentPath = os.getcwd()
+    os.chdir(srcPath[srcPathID]+'/classes')
+    try:
+        execfile('class_coupon_generic.py')
+        execfile('class_'+template.lower()+'.py')
+        couponClass = getattr(sys.modules[__name__], template.lower())
+    except:
+        pass
+    os.chdir(currentPath)
+
     self = couponClass(couponData)
-
-    if os.path.exists(srcPath[srcPathID]+'/classes/class_'+template.lower()+'.pyc'):
-        os.remove(srcPath[srcPathID]+'/classes/class_'+template.lower()+'.pyc')
-
-    if os.path.isdir(srcPath[srcPathID]+'/classes/__pycache__'):
-        shutil.rmtree(srcPath[srcPathID]+'/classes/__pycache__')
 except Exception as err:
     print(str(err))
+    self = None
 
