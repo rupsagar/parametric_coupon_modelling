67c56d90b91aedf71a45e8d0d4524275d208ee46
Rupsagar Chatterjee <22033397+rupsagar@users.noreply.github.com>
2023-02-16 10:24:11 +0530

    v2

--- Scripts/src/main_file.py
+++ Scripts/src/main_file.py
@@ -10,3 +10,4 @@ srcPath = os.getcwd()
 guiModule = imp.load_source('class_coupon_gui', srcPath+'/classes/class_coupon_gui.py')
 guiClass = getattr(guiModule, 'coupon_gui')
 newGUI = guiClass(abqPath, srcPath)
+
