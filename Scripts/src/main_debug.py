32b776e2e99887143615911c8aa9917fe42f0ca3
Rupsagar Chatterjee <22033397+rupsagar@users.noreply.github.com>
2023-02-22 08:31:54 +0530

    v2

--- Scripts/src/main_debug.py
+++ Scripts/src/main_debug.py
@@ -3,26 +3,35 @@
 srcPath = [r'Z:\Rupsagar\04_Coupon_Parametric_Modelling\01_WIP\Scripts\src', 
         r'D:\Programming\python\abaqus_scripts\Coupon_Parametric_Modelling\Scripts\src']
 
-template = 'Fatigue_Coupon_66_69_A'
-coupon = 'Fatigue_Coupon_66A'
+# template = 'Fatigue_Coupon_66_69_A'
+# coupon = 'Fatigue_Coupon_67A'
+
+# template = 'Fatigue_Coupon_66_69_BJ'
+# coupon = 'Fatigue_Coupon_66E'
+
+# template = 'Fatigue_Coupon_70_73_A'
+# coupon = 'Fatigue_Coupon_70A'
+
+template = 'Fatigue_Coupon_70_73_BE'
+coupon = 'Fatigue_Coupon_73E'
+
+# template = 'Fatigue_Coupon_74_75'
+# coupon = 'Fatigue_Coupon_74'
 
 # template = 'Static_Coupon_1_1_7'
-# coupon = 'Static_Coupon_1_5'
+# coupon = 'Static_Coupon_1_7'
 
 # template = 'Static_Coupon_2_1_6'
-# coupon = 'Static_Coupon_2_4'
+# coupon = 'Static_Coupon_2_6'
 
-
-import os
-import imp
+import os, sys
 
 for i in range(len(srcPath)):
     if os.path.isdir(srcPath[i]):
         srcPathID = i
         break
 
-debugModule = imp.load_source('util_debug', srcPath[srcPathID]+'/utils/util_debug.py')
-debugFunc = getattr(debugModule, 'debug')
-
+execfile(srcPath[srcPathID]+'/utils/util_debug.py')
+debugFunc = getattr(sys.modules[__name__], 'debug')
 self = debugFunc(srcPath[srcPathID], template, coupon)
 
