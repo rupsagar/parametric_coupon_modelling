834f0ac63d1b992739750e2deced8dd73366fbde
Rupsagar Chatterjee <22033397+rupsagar@users.noreply.github.com>
2023-03-03 15:43:09 +0530

    v3

--- Scripts/src/main_debug.py
+++ Scripts/src/main_debug.py
@@ -1,27 +1,28 @@
 ## Modify the template and coupon lines for debugging purpose and copy+paste or run the script in Abaqus CLI
 ## The srcPath list contains possible paths for src folder which may need to be updated based on where the script is located
 srcPath = [r'Z:\Rupsagar\04_Coupon_Parametric_Modelling\01_WIP\Scripts\src', 
-        r'D:\Programming\python\abaqus_scripts\Coupon_Parametric_Modelling\Scripts\src']
+        r'D:\Programming\python\abaqus_scripts\Coupon_Parametric_Modelling\Scripts\src',
+        r'\\fr0-svm21\St_RnT_METAL3\01-SIMU_STnS_USE_CASES\13_A350F\09_Coupon_Parametric_Modelling\Scripts\src']
 
-# template = 'Fatigue_Coupon_66_69_A'
-# coupon = 'Fatigue_Coupon_67A'
+template = 'Fatigue_Coupon_66_69_A'
+coupon = 'Fatigue_Coupon_66A'
 
 # template = 'Fatigue_Coupon_66_69_BJ'
-# coupon = 'Fatigue_Coupon_66E'
+# coupon = 'Fatigue_Coupon_69E'
 
 # template = 'Fatigue_Coupon_70_73_A'
-# coupon = 'Fatigue_Coupon_70A'
+# coupon = 'Fatigue_Coupon_73A'
 
-template = 'Fatigue_Coupon_70_73_BE'
-coupon = 'Fatigue_Coupon_73E'
+# template = 'Fatigue_Coupon_70_73_BE'
+# coupon = 'Fatigue_Coupon_73E'
 
 # template = 'Fatigue_Coupon_74_75'
 # coupon = 'Fatigue_Coupon_74'
 
-# template = 'Static_Coupon_1_1_7'
+# template = 'Static_Coupon_1_1_7_Symmetric'
 # coupon = 'Static_Coupon_1_7'
 
-# template = 'Static_Coupon_2_1_6'
+# template = 'Static_Coupon_2_1_6_Symmetric'
 # coupon = 'Static_Coupon_2_6'
 
 import os, sys
