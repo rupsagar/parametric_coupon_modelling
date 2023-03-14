ed63876ef1246e374ceb4ec26929446bc93b0974
Rupsagar Chatterjee <22033397+rupsagar@users.noreply.github.com>
2023-03-14 20:50:21 +0530

    v2

--- Scripts/src/classes/class_static_coupon_2_full_1_6.py
+++ Scripts/src/classes/class_static_coupon_2_full_1_6.py
@@ -38,12 +38,12 @@ class static_coupon_2_full_1_6(coupon_generic):
         self.createProfileSketch()
         self.createPart()
         self.createAssembly()
-        # self.createPartition()
-        # self.createMesh()
-        # self.createMaterial()
-        # self.createSection()
-        # self.createStep()
-        # self.createJob()
+        self.createPartition()
+        self.createMesh()
+        self.createMaterial()
+        self.createSection()
+        self.createStep()
+        self.createJob()
     def createProfileSketch(self):
         ## method to draw sketch of coupon profile
         ## calculate vertex coordinates #################################################################################################################
@@ -139,8 +139,14 @@ class static_coupon_2_full_1_6(coupon_generic):
         (self.xE, self.yE) = self.profileVertices1[5].coords
         (self.xF, self.yF) = self.profileVertices1[6].coords
         (self.xG, self.yG) = self.profileVertices1[7].coords
-        (self.xH, self.yH) = self.profileVertices1[8].coords
-        (self.xC2, self.yC2) = self.profileVertices1[9].coords
+        (self.xC2, self.yC2) = self.profileVertices1[8].coords
+        (self.xH, self.yH) = self.profileVertices1[9].coords
+        (self.xI, self.yI) = self.profileVertices1[10].coords
+        (self.xC3, self.yC3) = self.profileVertices1[11].coords
+        (self.xJ, self.yJ) = self.profileVertices1[12].coords
+        (self.xK, self.yK) = self.profileVertices1[13].coords
+        (self.xL, self.yL) = self.profileVertices1[14].coords
+        (self.xC4, self.yC4) = self.profileVertices1[15].coords
         #######################################################################################################################################################
         self.geomData = [['O', self.xo, self.yo, self.xO, self.yO],
                          ['A', self.xa, self.ya, self.xA, self.yA],
@@ -151,17 +157,25 @@ class static_coupon_2_full_1_6(coupon_generic):
                          ['F', self.xf, self.yf, self.xF, self.yF],
                          ['G', self.xg, self.yg, self.xG, self.yG],
                          ['H', self.xh, self.yh, self.xH, self.yH],
+                         ['I', self.xi, self.yi, self.xI, self.yI],
+                         ['J', self.xj, self.yj, self.xJ, self.yJ],
+                         ['K', self.xk, self.yk, self.xK, self.yK],
+                         ['L', self.xl, self.yl, self.xL, self.yL],
                          ['Center1', self.xc1, self.yc1, self.xC1, self.yC1],
                          ['Center2', self.xc2, self.yc2, self.xC2, self.yC2],
                          ['lt', '', self.lt, '', (self.xE-self.xF)],
-                         ['b', '', self.b, '', 2*self.yA],
-                         ['B1', '', self.B, '', 2*(self.yD-self.yE)],
-                         ['B2', '', self.B, '', 2*(self.yG-self.yF)],
+                         ['b1', '', self.b, '', (self.yA-self.yH)],
+                         ['b2', '', self.b, '', (self.yB-self.yG)],
+                         ['B1', '', self.B, '', (self.yD-self.yE)],
+                         ['B2', '', self.B, '', (self.yK-self.yJ)],
                          ['R1', '', self.R, '', ((self.xC1-self.xC)**2+(self.yC1-self.yC)**2)**0.5],
-                         ['R2', '', self.R, '', ((self.xC2-self.xH)**2+(self.yC2-self.yH)**2)**0.5],
+                         ['R2', '', self.R, '', ((self.xC2-self.xF)**2+(self.yC2-self.yF)**2)**0.5],
+                         ['R3', '', self.R, '', ((self.xC3-self.xI)**2+(self.yC3-self.yI)**2)**0.5],
+                         ['R4', '', self.R, '', ((self.xC4-self.xL)**2+(self.yC4-self.yL)**2)**0.5],
                          ['C1', '', self.C, '', (self.xD-self.xC)],
-                         ['C2', '', self.C, '', (self.xH-self.xG)],
-                         ['lc', '', self.lc, '', (self.xB-self.xA)]]
+                         ['C2', '', self.C, '', (self.xL-self.xK)],
+                         ['lc1', '', self.lc, '', (self.xB-self.xA)],
+                         ['lc2', '', self.lc, '', (self.xG-self.xH)]]
     def createPart(self):
         ## create solid
         self.part = len(self.profileSketch)*[None]
@@ -181,34 +195,55 @@ class static_coupon_2_full_1_6(coupon_generic):
             ## partition by YZ plane
             self.datumPlane_ID = self.part[0].DatumPlaneByPrincipalPlane(principalPlane=YZPLANE, offset=offsetDistance).id
             self.part[0].PartitionCellByDatumPlane(datumPlane=self.part[0].datums[self.datumPlane_ID], cells=self.part[0].cells)
+        createPartitionLong(self.xO)
+        createPartitionLong(self.xL)
+        createPartitionLong(self.xA)
         createPartitionLong(self.xB)
         createPartitionLong(self.xC)
+        ## partition by XZ plane
+        self.datumPlaneXZ_ID = self.part[0].DatumPlaneByPrincipalPlane(principalPlane=XZPLANE, offset=self.yO).id
+        self.part[0].PartitionCellByDatumPlane(datumPlane=self.part[0].datums[self.datumPlaneXZ_ID], cells=self.part[0].cells)
     def createMesh(self):
         ## seed ==>> thickness direction
         edgesThickness = self.part[0].edges.findAt(coordinates=((0, 0, self.lenTol), ))
         self.part[0].seedEdgeBySize(edges=edgesThickness, size=self.seedSizeThickness, deviationFactor=0.1, constraint=FINER)
         ## seed ==>> vertical direction
-        edgesVertical = self.part[0].edges.findAt(coordinates=((0, self.lenTol, 0), ))
-        self.part[0].seedEdgeBySize(edges=edgesVertical, size=self.seedSizeVertical, deviationFactor=0.1, constraint=FINER)
+        edgesVertical1 = self.part[0].edges.findAt(coordinates=((0, self.lenTol, 0), ))
+        self.part[0].seedEdgeBySize(edges=edgesVertical1, size=self.seedSizeVertical, deviationFactor=0.1, constraint=FINER)
+        edgesVertical2 = self.part[0].edges.findAt(coordinates=((0, -self.lenTol, 0), ))
+        self.part[0].seedEdgeBySize(edges=edgesVertical2, size=self.seedSizeVertical, deviationFactor=0.1, constraint=FINER)
         ## seed ==>> long edges along AB
-        pickedEdges3 = self.part[0].edges.getByBoundingBox(xMin=self.xA-self.lenTol, yMin=-self.lenTol, zMin=-self.lenTol, xMax=self.xB+self.lenTol, yMax=self.yB+self.lenTol, zMax=self.thickness+self.lenTol)
-        edgesLong3 = self.getEdgeByLength(pickedEdges3, abs(self.xB-self.xA))
-        self.seedEdge(self.part[0], 0, self.xA, edgesLong3, minSize=self.seedSizeLong1, maxSize=self.seedSizeLong2)
+        pickedEdges1 = self.part[0].edges.getByBoundingBox(xMin=self.xH-self.lenTol, yMin=self.yH-self.lenTol, zMin=-self.lenTol, xMax=self.xB+self.lenTol, yMax=self.yB+self.lenTol, zMax=self.thickness+self.lenTol)
+        edgesLong1 = self.getEdgeByLength(pickedEdges1, abs(self.xB-self.xO))
+        self.seedEdge(self.part[0], 0, self.xO, edgesLong1, minSize=self.seedSizeLong1, maxSize=self.seedSizeLong2)
         ## seed ==>> long edges along BC
-        pickedEdges1 = self.part[0].edges.getByBoundingBox(xMin=self.xB-self.lenTol, yMin=-self.lenTol, zMin=-self.lenTol, xMax=self.xC+self.lenTol, yMax=self.lenTol, zMax=self.thickness+self.lenTol)
-        edgesLong1 = self.getEdgeByLength(pickedEdges1, abs(self.xC-self.xB))
+        edgesLong1 = self.part[0].edges.findAt(coordinates=((self.xB+self.lenTol, 0, 0), (self.xB+self.lenTol, 0, self.thickness), 
+                                                              (self.xA-self.lenTol, 0, 0), (self.xA-self.lenTol, 0, self.thickness)))
         self.seedEdge(self.part[0], 0, self.xB, edgesLong1, minSize=self.seedSizeLong2, maxSize=self.seedSizeLong3)
+        self.seedEdge(self.part[0], 0, self.xA, edgesLong1, minSize=self.seedSizeLong2, maxSize=self.seedSizeLong3)
         ## seed ==>> arc BC
-        edgesTemp = self.part[0].edges.findAt(coordinates=((self.xB+self.lenTol, 0, 0), ))
-        ratioBias = self.part[0].getEdgeSeeds(edgesTemp[0], attribute=BIAS_RATIO)
-        elemNum = self.part[0].getEdgeSeeds(edgesTemp[0], attribute=NUMBER)
-        edgesTemp2 = self.part[0].edges.getByBoundingBox(xMin=self.xB-self.lenTol, yMin=-self.lenTol, zMin=-self.lenTol, xMax=self.xC+self.lenTol, yMax=self.yC+self.lenTol, zMax=self.thickness+self.lenTol)
-        edgesArc = self.getArcEdge(edgesTemp2)
-        self.seedEdge(self.part[0], 0, self.xB, edgesArc, ratio=ratioBias, number=elemNum)
+        edgesTemp1 = self.part[0].edges.findAt(coordinates=((self.xB+self.lenTol, 0, 0), ))
+        ratioBias = self.part[0].getEdgeSeeds(edgesTemp1[0], attribute=BIAS_RATIO)
+        elemNum = self.part[0].getEdgeSeeds(edgesTemp1[0], attribute=NUMBER)
+        edgesTemp2 = self.part[0].edges.getByBoundingBox(xMin=self.xG-self.lenTol, yMin=self.yF-self.lenTol, zMin=-self.lenTol, xMax=self.xC+self.lenTol, yMax=self.yC+self.lenTol, zMax=self.thickness+self.lenTol)
+        edgesArc1 = self.getArcEdge(edgesTemp2)
+        self.seedEdge(self.part[0], 0, self.xB, edgesArc1, ratio=ratioBias, number=elemNum)
+        ## seed ==>> arc AL
+        edgesTemp3 = self.part[0].edges.findAt(coordinates=((self.xA-self.lenTol, 0, 0), ))
+        ratioBias = self.part[0].getEdgeSeeds(edgesTemp3[0], attribute=BIAS_RATIO)
+        elemNum = self.part[0].getEdgeSeeds(edgesTemp3[0], attribute=NUMBER)
+        edgesTemp4 = self.part[0].edges.getByBoundingBox(xMin=self.xI-self.lenTol, yMin=self.yI-self.lenTol, zMin=-self.lenTol, xMax=self.xA+self.lenTol, yMax=self.yL+self.lenTol, zMax=self.thickness+self.lenTol)
+        edgesArc2 = self.getArcEdge(edgesTemp4)
+        self.seedEdge(self.part[0], 0, self.xA, edgesArc2, ratio=ratioBias, number=elemNum)
         ## seed ==>> long edges along CD
-        pickedEdges3 = self.part[0].edges.getByBoundingBox(xMin=self.xC-self.lenTol, yMin=-self.lenTol, zMin=-self.lenTol, xMax=self.xD+self.lenTol, yMax=self.yD+self.lenTol, zMax=self.thickness+self.lenTol)
-        edgesLong3 = self.getEdgeByLength(pickedEdges3, abs(self.xD-self.xC))
+        edgesLong3 = self.part[0].edges.findAt(coordinates=((self.xC+self.lenTol, self.yC, 0), (self.xC+self.lenTol, self.yC, self.thickness), 
+                                                            (self.xF+self.lenTol, self.yF, 0), (self.xF+self.lenTol, self.yF, self.thickness), 
+                                                            (self.xC+self.lenTol, self.yO, 0), (self.xC+self.lenTol, self.yO, self.thickness), 
+                                                            (self.xL-self.lenTol, self.yL, 0), (self.xL-self.lenTol, self.yL, self.thickness), 
+                                                            (self.xI-self.lenTol, self.yI, 0), (self.xI-self.lenTol, self.yI, self.thickness),
+                                                            (self.xL-self.lenTol, self.yO, 0), (self.xL-self.lenTol, self.yO, self.thickness), ))
         self.seedEdge(self.part[0], 0, self.xC, edgesLong3, minSize=self.seedSizeLong3, maxSize=self.seedSizeLong4)
+        self.seedEdge(self.part[0], 0, self.xL, edgesLong3, minSize=self.seedSizeLong3, maxSize=self.seedSizeLong4)
         ## set element types
         elemType1 = mesh.ElemType(elemCode=self.elemTypeHexPart1, elemLibrary=STANDARD)
         self.part[0].setElementType(regions=(self.part[0].cells,), elemTypes=(elemType1, ))
@@ -222,31 +257,16 @@ class static_coupon_2_full_1_6(coupon_generic):
         self.model.StaticStep(name='Load', previous='Initial', nlgeom=self.nlGeom, initialInc=self.initIncr, timePeriod=1.0, minInc=1e-4, maxInc=1.0)
         self.model.fieldOutputRequests['F-Output-1'].setValues(variables=('S', 'U', 'RF'))
         self.couponData['step'].update({'endPressure':self.endStress})
-        for i in range(len(self.part)):
-            ## create BC at negY face
-            nodesNegY = self.part[i].nodes.getByBoundingBox(xMin=self.xA-self.lenTol, yMin=-self.lenTol, zMin=-self.lenTol, xMax=self.xD+self.lenTol, yMax=self.lenTol, zMax=self.thickness+self.lenTol)
-            nsetNameNegY = 'Nset_NegY_Part_'+str(i+1)
-            self.part[i].Set(nodes=nodesNegY, name=nsetNameNegY)
-            region = self.instance[i].sets[nsetNameNegY]
-            self.model.DisplacementBC(name='BC_NegY_Instance_'+str(i+1), createStepName='Load', region=region, u1=UNSET, u2=SET, u3=UNSET, ur1=UNSET, ur2=UNSET, ur3=UNSET, amplitude=UNSET, distributionType=UNIFORM, fieldName='', localCsys=None)
-            ## create BC at negZ face
-            nodesPosZ = self.part[i].nodes.getByBoundingBox(xMin=self.xA-self.lenTol, yMin=-self.lenTol, zMin=-self.lenTol, xMax=self.xD+self.lenTol, yMax=self.yD+self.lenTol, zMax=self.lenTol)
-            nsetNamePosZ = 'Nset_NegZ_Part_'+str(i+1)
-            self.part[i].Set(nodes=nodesPosZ, name=nsetNamePosZ)
-            region = self.instance[i].sets[nsetNamePosZ]
-            self.model.DisplacementBC(name='BC_NegZ_Instance_'+str(i+1), createStepName='Load', region=region, u1=UNSET, u2=UNSET, u3=SET, ur1=UNSET, ur2=UNSET, ur3=UNSET, amplitude=UNSET, distributionType=UNIFORM, fieldName='', localCsys=None)
-            if i==0:
-                ## create BC at negX face
-                nodesNegX = self.part[i].nodes.getByBoundingBox(xMin=self.xA-self.lenTol, yMin=-self.lenTol, zMin=-self.lenTol, xMax=self.xA+self.lenTol, yMax=self.yA+self.yC+self.lenTol, zMax=self.thickness+self.lenTol)
-                nsetNameNegX = 'Nset_NegX_Part_'+str(i+1)
-                self.part[i].Set(nodes=nodesNegX , name=nsetNameNegX)
-                region = self.instance[i].sets[nsetNameNegX]
-                self.model.DisplacementBC(name='BC_NegX_Instance_'+str(i+1), createStepName='Load', region=region, u1=SET, u2=UNSET, u3=UNSET, ur1=UNSET, ur2=UNSET, ur3=UNSET, amplitude=UNSET, fixed=OFF, distributionType=UNIFORM, fieldName='', localCsys=None)
-            if i==(len(self.part)-1):
-                ## create pressure load on posX face
-                endCellFaceArr = self.part[i].faces.getByBoundingBox(xMin=self.xD-self.lenTol, yMin=-self.lenTol, zMin=-self.lenTol, xMax=self.xD+self.lenTol, yMax=self.yD+self.lenTol, zMax=self.thickness+self.lenTol)
-                surfNamePosX = 'Surf_PosX_Part_'+str(i+1)
-                self.getElemSurfFromCellFace(self.part[i], endCellFaceArr, surfNamePosX)
-                region = self.instance[i].surfaces[surfNamePosX]
-                self.model.Pressure(name='Load_PosX_Instance_'+str(i+1), createStepName='Load', region=region, distributionType=UNIFORM, field='', magnitude=self.endStress, amplitude=UNSET)
-                
+        ## create pressure load on posX face
+        endCellFaceArr1 = self.part[0].faces.getByBoundingBox(xMin=self.xE-self.lenTol, yMin=self.yE-self.lenTol, zMin=-self.lenTol, xMax=self.xD+self.lenTol, yMax=self.yD+self.lenTol, zMax=self.thickness+self.lenTol)
+        surfNamePosX = 'Surf_PosX_Part_'+str(0+1)
+        self.getElemSurfFromCellFace(self.part[0], endCellFaceArr1, surfNamePosX)
+        region1 = self.instance[0].surfaces[surfNamePosX]
+        self.model.Pressure(name='Load_PosX_Instance_'+str(0+1), createStepName='Load', region=region1, distributionType=UNIFORM, field='', magnitude=self.endStress, amplitude=UNSET)
+        ## create pressure load on negX face
+        endCellFaceArr2 = self.part[0].faces.getByBoundingBox(xMin=self.xJ-self.lenTol, yMin=self.yJ-self.lenTol, zMin=-self.lenTol, xMax=self.xK+self.lenTol, yMax=self.yK+self.lenTol, zMax=self.thickness+self.lenTol)
+        surfNameNegX = 'Surf_NegX_Part_'+str(0+1)
+        self.getElemSurfFromCellFace(self.part[0], endCellFaceArr2, surfNameNegX)
+        region2 = self.instance[0].surfaces[surfNameNegX]
+        self.model.Pressure(name='Load_NegX_Instance_'+str(0+1), createStepName='Load', region=region2, distributionType=UNIFORM, field='', magnitude=self.endStress, amplitude=UNSET)
+
