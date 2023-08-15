3ffa861cb394065ad5019e6c4c20c96987ed060d
Rupsagar Chatterjee <22033397+rupsagar@users.noreply.github.com>
2023-08-15 23:30:04 +0530

    v11
    before the major update

--- Scripts/src/main_debug.py
+++ Scripts/src/main_debug.py
@@ -51,8 +51,11 @@ srcPath = [r'Z:\Rupsagar\04_Coupon_Parametric_Modelling\01_WIP\Scripts\src',
 # template = 'Static_Coupon_18_21'
 # coupon = 'Static_Coupon_19'
 
-template = 'Fracture_Toughness_Coupon_22_36'
-coupon = 'Fracture_Toughness_Coupon_22'
+# template = 'Fracture_Coupon_22_36'
+# coupon = 'Fracture_Coupon_30'
+
+template = 'Crack_Growth_Coupon_44_50'
+coupon = 'Crack_Growth_Coupon_47'
 
 import os, sys
 
